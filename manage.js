02b813d7de165d734deee61c43af1a554a4b7639
Patrick Porto <patrick.s.porto@gmail.com>
2014-03-06 13:10:24 -0300

    mudança estrutural

---
+++ manage.js
@@ -0,0 +1,33 @@
+require('./lib/system/utils');
+
+var application = require('./lib/system/app'),
+	fs = require('fs'),
+	path = require('path');
+
+
+fs.readdirSync('./controllers').forEach(function(controller) {
+	if(path.extname(controller) == '.js') {
+		route = require('./controllers/' + controller);
+		route(application);
+	}
+});
+
+// console.log("validating models\n");
+
+var date = new Date().format("F d, Y - H:i:s");
+console.log(date);
+console.log("Nomerce version 1.0.0");
+
+var server = application.listen(3000, function() {
+	console.log('Starting express server at http://' + this.address().address + ":" + this.address().port);
+	console.log('Quit the server with CONTROL-C.');
+});
+
+server.on('error', function ( err ) {
+	if (err['code'] == 'EADDRINUSE') {
+		console.log('Error: That port is already in use.');
+	} else {
+		console.log(err);
+	}
+    process.exit(1);
+});
